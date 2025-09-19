ff04591a71421021dc553d4fec59730b8d89fc2d
Sergio <sergiomsrs@gmail.com>
2025-09-19 06:30:08 +0200

    Fin de la seccion 18. Tienda finalizada sin paginacion

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+
+
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+
+  productSlug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productSlug }),
+    loader: ({ request }) => {
+      return this.productService.getProductByIdSlug(request.idSlug)
+    }
+  })
+
+}
